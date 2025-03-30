4cbfe5385a0f43160a64b4607d8376ccbacf4b61
Juan Vitório <juanvitorioclash@gmail.com>
2025-03-30 16:24:13 -0300

    Adicionado a opção de busca

--- gerencia.py
+++ gerencia.py
@@ -3,17 +3,18 @@ estoque = { #dicionário
     1:{'nome': 'banana', 'quantidade': 1, 'valor':2.5}
 }
 
-def listar_itens():
+def listarItens():
     if not estoque: # caso estoque seja null, ele informará e irá retornar.
         print('Não há itens')
         return
+    
     for chave, item in estoque.items(): #eu percorro os itens nos valores do dicionário de estoque e printo eles. #items me retorna uma tupla, ou seja, ela me retorna o ID e o valor referente ao ID, por isso preciso usar duas var para percorrer os itens do dic
         print(f"ID: {chave} | Nome: {item['nome']} | Quantidade: {item['quantidade']} | Valor: {item['valor']}")
     return
 
-def adicionar_item():
+def adicionarItem():
     novo_id = max(estoque.keys(), default=-1) + 1 #a função Max pega o maior valor de uma estrutura de dados, dentro dela, eu uso o keys() pra pegar as chave do dicionário. Tendo o maior valor do dicionários, eu adiciono + 1 para fazer o novo id. Caso esteja vazio, começa em 0
-    nome = input("Qual o nome do item: ")
+    nome = input("Qual o nome do item: ").lower()
     quantidade = int(input(f"Quantidade de {nome} em estoque: "))
     valor = float(input(f"Valor de {nome}: "))
     estoque[novo_id] = { #adicionando o item ao dicionário
@@ -25,37 +26,63 @@ def adicionar_item():
     print(estoque[novo_id])  # Mostra o item adicionado
     return
 
-def remover_item():
-    listar_itens()
+def removerItem():
+    listarItens()
     chave = int(input("Digite a chave do item que deseja remover: "))
+
     if(chave not in estoque.keys()):
         print("Essa chave não existe no estoque")
         return
+    
     del estoque[chave]
     print("Item removido com sucesso!")
     return
 
-def editar_item():
-    listar_itens()
+def editarItem():
+    listarItens()
     chave = int(input("Que ID deseja editar: "))
+
     if(chave not in estoque.keys()):
         print("Essa ID não existe no estoque")
         return
+    
     print(f"| ID 0:Nome - {estoque[chave]['nome']} | ID 1:Quantidade - {estoque[chave]['quantidade']} | ID 2:Valor - {estoque[chave]['valor']}")
     op = int(input("Qual ID deseja editar: "))
+
     if(op < 0 or op > 2):
         print("Esse ID não existe nas opções")
         return
+    
     if(op == 0):
-        nome = input("Digite o nome: ")
+        nome = input("Digite o nome: ").lower()
         estoque[chave]['nome'] = nome
+        
     elif(op == 1):
         quantidade = int(input("Digite a quantidade: "))
         estoque[chave]['quantidade'] = quantidade
+
     elif(op == 2):
         valor = float(input("Digite o valor: "))
         estoque[chave]['valor'] = valor
+
     print("Item alterado com sucesso!")
+    
+def buscaItem():
+    if not estoque:
+        print("O estoque está vazio.")
+        return
+    
+    busca = input("O que está buscando: ").lower()
+    encontrado = False  # Flag para verificar se algum item foi encontrado
+
+    for chave, item in estoque.items():
+        if busca == item['nome']:
+            print(f"ID: {chave} | Nome: {item['nome']} | Quantidade: {item['quantidade']} | Valor: {item['valor']}")
+            encontrado = True
+    
+    if not encontrado:
+        print(f"Não há '{busca}' no estoque.")
+
 
 def menu():
     while True:
@@ -64,21 +91,30 @@ def menu():
         2 - Adicionar item
         3 - Remover item
         4 - Editar item
+        5 - Buscar item
         0 - Sair
         """)
         op = input("Sua opção: ")  
         if op == "1":
-            listar_itens()
+            listarItens()
             input("Aperte enter para voltar")
+
         elif op == "2":
-            adicionar_item()
+            adicionarItem()
             input("Aperte enter para voltar")
+
         elif op == "3":
-            remover_item()
+            removerItem()
             input("Aperte enter para voltar")
+
         elif op == "4":
-            editar_item()
+            editarItem()
             input("Aperte enter para voltar")
+
+        elif op == "5":
+            buscaItem()
+            input("Aperte enter para voltar")
+            
         elif op == "0":
             print("Saindo...")
             break  # Encerra o loop corretamente
